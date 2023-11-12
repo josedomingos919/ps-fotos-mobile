a70af3c2b08d4332914a96c83a9fe28f351a82ac
josedomingos919 <josedomingos919@gmail.com>
2023-11-12 11:01:50 +0100

    feat: add state

--- src/screens/my-album/index.js
+++ src/screens/my-album/index.js
@@ -1,6 +1,10 @@
+import { useMyAlbumScreenState } from "./state";
+
 import * as S from "./style";
 
 export const MyAlbumScreen = () => {
+  const {} = useMyAlbumScreenState();
+
   return (
     <S.Container>
       <S.Title marginBottom={55}>Meus Albuns</S.Title>
